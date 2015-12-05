f06ab7eec4424e1b2676e0e89bc0dc80a4ea2efb
Oleg Grigoriev <go.vasac@gmail.com>
2015-12-05 12:23:01 +0300

    Ref Travis

--- cs.sh
+++ cs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cmd="vendor/bin/phpcs --standard=PSR2 --encoding=utf-8 --ignore=vendor"
+cmd="/usr/bin/env phpcs --standard=PSR2 --encoding=utf-8 --ignore=vendor"
 
 if [ "$#" -ne 0 ]; then
 $cmd "$@"
